853d3fe4ae724b94cca02b24e1187743f244aab4
tarex7 <tarex7@gmail.com>
2022-07-06 14:24:57 +0200

    Milestone 1 done

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const posts = [
+  {
+    id: 1,
+    author: "Quentin Tarantino",
+    authorImg: "https://source.unsplash.com/random/",
+    date: "08/07/2022",
+    postText:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea rem in quas. Aliquam eos ipsum, suscipit expedita provident fugiat nihil quam quas possimus iusto nemo in doloremque aliquid, minima rerum!",
+    postImg: "https://source.unsplash.com/random/",
+    likes: 23,
+  },
+  {
+    id: 1,
+    author: "Isaac Newton",
+    authorImg: "https://source.unsplash.com/random/",
+    date: "10/27/2022",
+    postText:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea rem in quas. Aliquam eos ipsum, suscipit expedita provident fugiat nihil quam quas possimus iusto nemo in doloremque aliquid, minima rerum!",
+    postImg: "https://source.unsplash.com/random/",
+    likes: 109,
+  },
+  {
+    id: 1,
+    author: "Eddie Vedder",
+    authorImg: "https://source.unsplash.com/random/",
+    date: "03/11/2022",
+    postText:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea rem in quas. Aliquam eos ipsum, suscipit expedita provident fugiat nihil quam quas possimus iusto nemo in doloremque aliquid, minima rerum!",
+    postImg: "https://source.unsplash.com/random/",
+    likes: 62,
+  },
+  {
+    id: 1,
+    author: "Elon Musk",
+    authorImg: "https://source.unsplash.com/random/",
+    date: "02/20/2022",
+    postText:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea rem in quas. Aliquam eos ipsum, suscipit expedita provident fugiat nihil quam quas possimus iusto nemo in doloremque aliquid, minima rerum!",
+    postImg: "https://source.unsplash.com/random/",
+    likes: 79,
+  },
+];
