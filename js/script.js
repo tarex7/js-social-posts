d151a0761bb2732172d1572772e16036dea59993
tarex7 <tarex7@gmail.com>
2022-07-06 23:09:48 +0200

    Bonus 1/2

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const posts = [
     authorImg: "https://source.unsplash.com/random/100x100/?animals/",
     date: dateTransform("08/07/2022"),
     postText:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea rem in quas. Aliquam eos ipsum, suscipit expedita provident fugiat nihil quam quas possimus iusto nemo in doloremque aliquid, minima rerum!",
+      "“Young people - they care. They know that this is the world that they're going to grow up in, that they're going to spend the rest of their lives in. But, I think it's more idealistic than that. They actually believe that humanity, human species, has no right to destroy and despoil regardless.”",
     postImg: "https://source.unsplash.com/random/400x400/?nature,wildlife",
     likes: 23,
     NamefirstLetters: getFirstLetters("David Attemborough"),
@@ -16,7 +16,7 @@ const posts = [
     authorImg: "https://source.unsplash.com/random/100x100/?space",
     date: dateTransform("10/27/2022"),
     postText:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea rem in quas. Aliquam eos ipsum, suscipit expedita provident fugiat nihil quam quas possimus iusto nemo in doloremque aliquid, minima rerum!",
+      `“We did not evolve to go to space, so when it comes to space travel, we are all disabled," says ESA astronaut Samantha Cristoforetti. "What brings us from being disabled to go to space to being able to go to space is just technology.”`,
     postImg: "https://source.unsplash.com/random/400x400/?space,earth",
     likes: 109,
     NamefirstLetters: getFirstLetters("Samantha Cristoforetti"),
@@ -27,7 +27,7 @@ const posts = [
     authorImg: "",
     date: dateTransform("03/11/2022"),
     postText:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea rem in quas. Aliquam eos ipsum, suscipit expedita provident fugiat nihil quam quas possimus iusto nemo in doloremque aliquid, minima rerum!",
+      "“Black. The song is about letting go. It's very rare for a relationship to withstand the Earth's gravitational pull and where it's going to take people and how they're going to grow. I've heard it said that you can't really have a true love unless it was a love unrequited. It's a harsh one, because then your truest one is the one you can't have forever.",
     postImg:
       "https://source.unsplash.com/random/400x400/?concert,rock,music,live",
     likes: 62,
@@ -39,7 +39,7 @@ const posts = [
     authorImg: "https://source.unsplash.com/random/100x100/?tesla/",
     date: dateTransform("02/20/2022"),
     postText:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea rem in quas. Aliquam eos ipsum, suscipit expedita provident fugiat nihil quam quas possimus iusto nemo in doloremque aliquid, minima rerum!",
+      "I really do encourage other manufacturers to bring electric cars to market. It's a good thing, and they need to bring it to market and keep iterating and improving and make better and better electric cars, and that's what going to result in humanity achieving a sustainable transport future. I wish it was growing faster than it is.",
     postImg: "https://source.unsplash.com/random/400x400/?tesla",
     likes: 79,
     NamefirstLetters: getFirstLetters("Elon Musk"),
@@ -50,7 +50,7 @@ const posts = [
     authorImg: "",
     date: dateTransform("02/20/2022"),
     postText:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea rem in quas. Aliquam eos ipsum, suscipit expedita provident fugiat nihil quam quas possimus iusto nemo in doloremque aliquid, minima rerum!",
+      "You have to start with the truth. The truth is the only way that we can get anywhere. Because any decision-making that is based upon a lie or ignorance can't lead to a good conclusion.",
     postImg: "https://source.unsplash.com/random/400x400/?assange",
     likes: 79,
     NamefirstLetters: getFirstLetters("Julian Assange"),
